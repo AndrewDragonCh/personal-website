7fc386e725ccc04521639a870201334c3f71aa6f
Andrew Still <astill@dragonaere.com>
2024-07-02 19:44:21 -0400

    Ahh

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
- 
+
 export function middleware(request: NextRequest) {
-  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https: http: 'unsafe-inline' ${
+      process.env.NODE_ENV === "production" ? "" : `'unsafe-eval'`
+    };
     style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data:;
     font-src 'self';
@@ -14,41 +16,33 @@ export function middleware(request: NextRequest) {
     frame-ancestors 'none';
     upgrade-insecure-requests;
 `
-  // Replace newline characters and spaces
+
   const contentSecurityPolicyHeaderValue = cspHeader
-    .replace(/\s{2,}/g, ' ')
-    .trim()
- 
-  const requestHeaders = new Headers(request.headers)
-  requestHeaders.set('x-nonce', nonce)
- 
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-nonce", nonce);
   requestHeaders.set(
-    'Content-Security-Policy',
-    contentSecurityPolicyHeaderValue
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue,
   )
- 
+
   const response = NextResponse.next({
     request: {
       headers: requestHeaders,
     },
   })
   response.headers.set(
-    'Content-Security-Policy',
-    contentSecurityPolicyHeaderValue
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue,
   )
- 
+
   return response
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
     {
       source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
       missing: [
