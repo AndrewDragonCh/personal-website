7555714d7b01bd3f4c2fb4daec2487f0260a5149
Andrew Still <astill@dragonaere.com>
2024-07-02 19:13:10 -0400

    Init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,62 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import remarkGfm from 'remark-gfm'
+import createMDX from '@next/mdx'
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'mdx'],
+  poweredByHeader: false,
+  crossOrigin: 'anonymous',
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+    ppr: true,
+    mdxRs: true,
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/blog',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(),midi=(),sync-xhr=(),microphone=(),camera=(),magnetometer=(),gyroscope=(),fullscreen=(self),payment=(),usb=()',
+          },
+        ],
+      },
+    ]
+  }
+}
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+})
+
+export default withMDX(nextConfig)
