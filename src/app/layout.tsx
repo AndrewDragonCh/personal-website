b5a47c49fc3c70dac83f880bb43e6699491fa212
Andrew Still <astill@dragonaere.com>
2024-07-02 22:06:44 -0400

    Added resume and basic link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,33 @@ import type { Metadata } from "next";
 import './globals.css'
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.andrewstill.moe"),
   title: "Andrew Still",
+  description: "Personal website for Andrew Still",
+  applicationName: "Andrew Still",
+  authors: [{ name: "Andrew Still" }],
+  keywords: ["Resume", "Developer", "Programmer"],
+  referrer: "no-referrer",
+  viewport: "width=device-width, initial-scale=1.0",
+  creator: "Andrew Still",
+  publisher: "Andrew Still",
+  formatDetection: {
+    telephone: true,
+    email: true,
+    address: false,
+    date: true,
+    url: true,
+  },
+  robots: "index, follow",
+  // Open Graph
+  openGraph: {
+    title: "Andrew Still",
+    description: "Personal website for Andrew Still",
+    type: "website",
+    locale: "en_US",
+    url: "https://andrewstill.moe",
+    siteName: "Andrew Still",
+  },
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Route } from "next";
 import { headers } from "next/headers";
+import Link from "next/link";
 import Script from "next/script";
 
 export default async function Home() {
@@ -7,8 +9,15 @@ export default async function Home() {
   return (
     <main>
       <div className="flex flex-col justify-center items-center h-screen" >
-        <h1 className="text-6xl m-4">Coming Soon</h1>
-        <p>Come back later</p>
+        <h1 className="text-8xl m-4">Andrew Still</h1>
+        <p className="text-3xl mt-1">Here are some quick links</p>
+        <div className="grid grid-cols-2 mt-4 place-items-center">
+          <Link className="text-2xl m-2" href={"https://www.linkedin.com/in/andrew-still-yay/"} target="_blank">LinkedIn</Link>
+          <Link className="text-2xl m-2" href={"resume070224.pdf" as Route} target="_blank" rel="noopener noreferrer" locale={false}>Resume</Link>
+          <Link className="text-2xl m-2" href={"https://github.com/AndrewDragonCh"} target="_blank">GitHub</Link>
+          <Link className="text-2xl m-2" href={"https://www.credly.com/badges/d4f09d4c-567e-4be3-a9b6-7242eeab0847"} target="_blank">CompTIA A+</Link>
+        </div>
+        <p className="mt-8">Please ignore the basic website, more to come.</p>
       </div>
       <script defer src="https://plausible.andrewstill.moe/js/script.js" data-domain="andrewstill.moe" nonce={nonce} />
     </main>
