3d3c548a099d2da44aa30c07ba185f0e875c9ad9
Andrew Still <astill@dragonaere.com>
2024-07-02 20:38:14 -0400

    Fixed CSP for plausible

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ export function middleware(request: NextRequest) {
   const cspHeader = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    connect-src 'self' 'nonce-${nonce}' https://plausible.andrewstill.moe;
     style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data:;
     font-src 'self';
